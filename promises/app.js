f4d9feeb77c119e793ac0e3dae52f2f889836858
hodal66 <mhthodol@gmail.com>
2023-03-12 03:18:45 +0200

    add what we callback hell

--- promises/app.js
+++ promises/app.js
@@ -15,18 +15,43 @@ const feckApiPromises = (url) => {
 let request = feckApiPromises("yelp.com/api/coffee/page1");
 request
   .then(() => {
-    console.log("PROMISE RESOLVED");
-    console.log("IT WORKED on page 1");
+    console.log("PROMISE WORKED ON PAG(1)");
     feckApiPromises("yelp.com/api/coffee/page2")
       .then(() => {
-        console.log("PROMISE WORKED ON PAGE 2");
-        console.log("it works again ON page 2");
+        console.log("PROMISE WORKED ON PAG(2)");
+        feckApiPromises("yelp.com/api/coffee/page3")
+          .then(() => {
+            console.log("PROMISE WORKED ON PAG(3)");
+            feckApiPromises("yelp.com/api/coffee/page4")
+              .then(() => {
+                console.log("PROMISE WORKED ON PAG(4)");
+                feckApiPromises("yelp.com/api/coffee/page5")
+                  .then(() => {
+                    console.log("PROMISE WORKED ON PAG(5)");
+                    feckApiPromises("yelp.com/api/coffee/page6")
+                      .then(() => {
+                        console.log("PROMISE WORKED ON PAG(6)");
+                      })
+                      .catch(() => {
+                        console.log("FAILED on page 6");
+                      });
+                  })
+                  .catch(() => {
+                    console.log("FAILED on page 5");
+                  });
+              })
+              .catch(() => {
+                console.log("FAILED on page 4");
+              });
+          })
+          .catch(() => {
+            console.log("FAILED on page 3");
+          });
       })
       .catch(() => {
-        console.log("FAILED AGAIN on page 2");
+        console.log("FAILED on page 2");
       });
   })
   .catch(() => {
-    console.log("PROMISE REJECTED");
-    console.log("IT FAILED");
+    console.log("FAILED on page 1");
   });
