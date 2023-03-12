22a4fbedc410d3d1d4c481bc5e6f4cea15f9df02
hodal66 <mhthodol@gmail.com>
2023-03-12 02:45:08 +0200

    promise worked successfully

--- promises/app.js
+++ promises/app.js
@@ -1,12 +1,32 @@
 const feckApiPromises = (url) => {
-  return new Promise((rejected, success) => {
-    delay = Math.floor(Math.random() * 4500) + 500;
+  return new Promise((rejected, resolved) => {
+    delay = Math.floor(Math.random() * 5000);
     setTimeout(() => {
       if (delay > 4000) {
-        rejected("App was Rejected!!");
+        resolved(`App was successfully rendered on ${url}`);
+        console.log(delay);
       } else {
-        success(`App was successfully rendered on ${url}`);
+        rejected("App was Rejected!!");
+        console.log(delay);
       }
     });
   });
 };
+let request = feckApiPromises("yelp.com/api/coffee/page1");
+request
+  .then(() => {
+    console.log("PROMISE RESOLVED");
+    console.log("IT WORKED on page 1");
+    feckApiPromises("yelp.com/api/coffee/page2")
+      .then(() => {
+        console.log("PROMISE WORKED ON PAGE 2");
+        console.log("it works again ON page 2");
+      })
+      .catch(() => {
+        console.log("FAILED AGAIN on page 2");
+      });
+  })
+  .catch(() => {
+    console.log("PROMISE REJECTED");
+    console.log("IT FAILED");
+  });
