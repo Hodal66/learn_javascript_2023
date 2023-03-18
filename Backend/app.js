b63e88f80f0203a04dd6852e7f09aa6be1d29257
hodal66 <mhthodol@gmail.com>
2023-03-18 11:10:29 +0200

    come on the backend side

---
+++ Backend/app.js
@@ -0,0 +1,3 @@
+const { addition, PI, square } = require("./math");
+console.log(addition(20, 30));
+console.log(PI);
---
+++ Backend/math.js
@@ -0,0 +1,20 @@
+module.exports.addition = (x, y) => {
+  return x + y;
+};
+
+module.exports.PI = 3.48487;
+module.exports.square = (x) => {
+  return x * x;
+};
+
+// module.exports.PI = PI;
+// module.exports.square = square;
+// module.exports.add = add;
+
+// const math = {
+//   addition: add,
+//   PI: PI,
+//   square: square,
+// };
+
+// module.exports = math;
