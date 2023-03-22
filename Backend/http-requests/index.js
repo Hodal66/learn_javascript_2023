7b3d67e606659b2353d1e2754841d4d5370a5c8d
hodal66 <mhthodol@gmail.com>
2023-03-22 09:35:17 +0200

    ft-get-post-requests(add basic routes)

---
+++ Backend/http-requests/index.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const server = express();
+const colors = require("colors");
+const port = 3050;
+
+server.use(express.urlencoded({ extended: true }));
+
+server.get("/", (req, res) => {
+  res.send("This home page that you have requested");
+});
+server.get("/ikijumba", (req, res) => {
+  res.send("THIS is get route you have requested");
+});
+server.post("/ikijumba", (req, res) => {
+  res.send("This is a post requests!!!");
+  const myName = req.body.name;
+  console.log(req.body);
+  if (myName === "Hodal") {
+    console.log("You passed the requestrs thank you ", myName);
+  } else {
+    console.log("You are not allowed to pass");
+  }
+});
+
+server.listen(port, (req, res) => {
+  console.log(
+    `Server of get and post is running at http://locallhost:${port}`.rainbow
+  );
+});
