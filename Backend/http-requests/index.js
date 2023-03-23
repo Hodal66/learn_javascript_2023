95e6a56f5e8be37afdda4160589b1ecea7e760d0
hodal66 <mhthodol@gmail.com>
2023-03-23 23:25:10 +0200

    add unsaved data

--- Backend/http-requests/index.js
+++ Backend/http-requests/index.js
@@ -4,6 +4,7 @@ const colors = require("colors");
 const port = 3050;
 
 server.use(express.urlencoded({ extended: true }));
+server.use(express.json());
 
 server.get("/", (req, res) => {
   res.send("This home page that you have requested");
@@ -12,13 +13,14 @@ server.get("/ikijumba", (req, res) => {
   res.send("THIS is get route you have requested");
 });
 server.post("/ikijumba", (req, res) => {
-  res.send("This is a post requests!!!");
   const myName = req.body.name;
   console.log(req.body);
   if (myName === "Hodal") {
     console.log("You passed the requestrs thank you ", myName);
+    res.send(`You passed the requests thank you , ${myName}`);
   } else {
     console.log("You are not allowed to pass");
+    res.send("You are not allowed to pass");
   }
 });
 
