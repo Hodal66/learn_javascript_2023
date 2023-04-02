791abf2da30ceba5a61ac185e694961c4e5144ea
hodal66 <mhthodol@gmail.com>
2023-04-02 12:30:18 +0200

    insert records into mongoos works well

--- Backend/mongoDB/index.js
+++ Backend/mongoDB/index.js
@@ -3,8 +3,20 @@ const app = express();
 const colors = require("colors");
 const port = 9000;
 const mongoose = require("mongoose");
+mongoose.connect("mongodb://127.0.0.1:27017/moviesApp");
 try {
-  mongoose.connect("mongodb://127.0.0.1:27017/test");
+  const moviesSchema = mongoose.Schema({
+    title: String,
+    score: Number,
+    isAvaillable: Boolean,
+    age: Number,
+    author: String,
+  });
+
+  const Movie = mongoose.model("Movie", moviesSchema);
+
+  const myFindings = Movie.find();
+  console.log("Ibyo Ni byoo Nishakiye Kuruhande:: ", myFindings);
   console.log("Waw Database Connected Successfully!!".rainbow);
 } catch (error) {
   console.log("Oh No There is an Error!!!");
